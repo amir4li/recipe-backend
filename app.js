a89ca31115f28b4080e9b956ca1ae5b97f790530
amir <aamir4ly@gmail.com>
2022-07-19 11:38:43 +0530

    hello

--- app.js
+++ app.js
@@ -1,29 +1,85 @@
 const express = require("express");
-const path = require("path");
-const ejs = require("ejs");
-const dotenv = require("dotenv");
+const apiRouter = require("./routes/apiRoutes");
+const importData = require("./data/import-quiz-data");
 
 const app = express();
-dotenv.config({ path: './config.env' });
-
-app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(express.urlencoded({ extended: true }));
 
 
-app.get("/login", (req, res)=> {
-    res.render("login");
+app.use(express.json());
+
+app.use((req, res, next)=> {
+    req.requestTime = new Date().toISOString();
+    next();
 });
 
-app.get("/signup", (req, res)=> {
-    res.render("signup");
-});
+// importData();
+
+// Routes
+app.use("/api", apiRouter);
+
+module.exports = app;
 
 
-const port = process.env.PORT;
-app.listen(port, ()=> {
-    console.log(`Server is running on port ${port}...`);
-});
+
+
+
+// app.post("/", async (req, res)=> {
+//     const category = req.body.category;
+//     console.log(category)
+
+//     const url = "https://opentdb.com/api.php?amount=07&category=18"
+
+//     // axios.get(url)
+//     // .then(response => {
+//     //     const data = response.data;
+//     //     res.render("game", {
+        
+//     //     });
+
+        
+//     // });
+//     axios.get('https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple', {
+//         'Access-Control-Allow-Origin': '*',
+//     })
+//         .then(() => {
+//             response.json();
+//             console.log(response);
+//         })
+//         // .then(res => {
+//         //     let id = 0;
+//         //     data = res.results.map(item => {
+//         //         // adding correct ans into options
+//         //         const options = [{
+//         //             text: item.correct_answer,
+//         //             isCorrect: true
+//         //         }];
+
+//         //         // adding all incorrect answers into options
+//         //         item.incorrect_answers.map(ans => {
+//         //             options.push({
+//         //                 text: ans,
+//         //                 isCorrect: false
+//         //             })
+//         //         });
+
+//         //         const resposne = {
+//         //             id: id++,
+//         //             question: item.question,
+//         //             options: options
+//         //         }
+
+//         //         // making simple object
+//         //         return resposne;
+//         //     });
+//         //     console.log(data);
+//         //     attachHandlers();
+//         //     generateQuestion(currentQuestion);
+        
+// });
+
+// // const port = process.env.PORT;
+// // app.listen(port, ()=> {
+// //     console.log(`Server is running on port ${port}...`);
+// // });
 
 
---
+++ public/js/game.js
@@ -0,0 +1,6 @@
+const category = document.querySelector("#category");
+const btn = document.querySelector("#btn");
+
+btn.addEventListener("click", ()=> {
+    console.log("hello");
+})
